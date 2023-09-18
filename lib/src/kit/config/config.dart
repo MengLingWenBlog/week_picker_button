614382f58745b8c7235bc4254474998b6a6a310e
孟令文 <848003640@qq.com>
2023-09-18 17:07:46 +0800

    0.0.3

--- lib/src/kit/config/config.dart
+++ lib/src/kit/config/config.dart
@@ -1,21 +1,3 @@
 class Config {
-  static const String NAME = "e积分";
-
-  // static late final String BRANCH;
-  //协议
-  static const String WEB_ABOUT = "http://flutterapp.sdhzm.tech/protocol/about.html";
-  static const String WEB_BUSINESS = "http://flutterapp.sdhzm.tech/protocol/business.html";
-  static const String WEB_UNSUBSCRIBE = "http://flutterapp.sdhzm.tech/protocol/unsubscribe.html";
-  static const String WEB_PRIVACY = "http://flutterapp.sdhzm.tech/protocol/privacy.html";
-  static const String WEB_SERVICE = "http://flutterapp.sdhzm.tech/protocol/service.html";
-
-  static const String TEL = "400-135-8388";
-  static const String COPYRIGHT =
-      "山东华之梦 版权所有 | Copyright © 2022-2023 Hzm.All Rights Reserved.";
-
-  //这个仅在以非web模式下运行的时候用
-  static const String API = "http://121.62.18.248:19010";
-  static const String USER_AGENT = "jifen-mock";
   static const bool BUILD_WEB = false;
-  static int VERSION = 0;
 }
